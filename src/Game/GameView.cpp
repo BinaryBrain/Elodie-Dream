2e4e2e917025a0b6c98d7498d88a48fbfffaf123
Jeremy Rabasco <jeremy.rabasco@epfl.ch>
2013-11-28 02:14:55 +0100

    Intermediate commit to get gravity modification

--- src/Game/GameView.cpp
+++ src/Game/GameView.cpp
@@ -13,29 +13,40 @@ sf::RenderWindow* GameView::getWindow() {
     return window;
 }
 
-void GameView::addDrawable(sf::Drawable* drawable) {
-    toDraw.push_back(drawable);
+void GameView::addDrawable(ViewLayer viewKey, sf::Drawable* drawable) {
+    toDrawMap[viewKey].push_back(drawable);
+}
+
+void GameView::addView(ViewLayer viewKey, sf::View* view){
+    viewMap[viewKey] = view;
+}
+
+void GameView::removeView(ViewLayer viewKey){
+    for(std::vector<ViewLayer>::iterator it = toDraw.begin(); it != toDraw.end(); ++it) {
+        if (*it == viewKey){
+            toDraw.erase(it);
+        }
+    }
 }
 
 void GameView::reset() {
     center = window->getDefaultView().getCenter();
 }
 
-void GameView::setCameraCenter(const sf::Vector2f* pos) {
-    setCameraCenter(pos->x, pos->y);
+void GameView::setCameraCenter(ViewLayer viewKey, const sf::Vector2f* pos) {
+    viewMap[viewKey]->setCenter(pos);
 }
 
-void GameView::setCameraCenter(float x, float y) {
-    this->center.x = x;
-    this->center.y = y;
+void GameView::setCameraCenter(ViewLayer viewKey, float x, float y) {
+    setCameraCenter(viewKey, &sf::Vector2f(x,y))
 }
 
-void GameView::setFollowedPoint(const sf::Vector2f* pos) {
-    setFollowedPoint(pos->x, pos->y);
+void GameView::setFollowedPoint(ViewLayer viewKey, const sf::Vector2f* pos) {
+    setFollowedPoint(viewKey, pos->x, pos->y);
 }
 
-void GameView::setFollowedPoint(float x, float y) {
-    sf::Vector2f viewSize = window->getView().getSize();
+void GameView::setFollowedPoint(ViewLayer viewKey, float x, float y) {
+    sf::Vector2f viewSize = viewMap[viewKey]->getSize();
 
     float margin = viewSize.y/4;
 
@@ -49,22 +60,21 @@ void GameView::setFollowedPoint(float x, float y) {
         camY = y-margin;
     }
 
-    setCameraCenter(camX, camY);
+    setCameraCenter(viewKey, camX, camY);
 }
 
 void GameView::draw() {
     sf::View view = window->getView();
-    view.setCenter(center);
     window->setView(view);
 
     window->clear(sf::Color(0x00, 0x00, 0xFF));
 
-    for(std::vector<sf::Drawable*>::iterator it = toDraw.begin(); it != toDraw.end(); ++it) {
+    for(std::vector<sf::Drawable*>::iterator it = toDrawOnMain.begin(); it != toDrawOnMain.end(); ++it) {
         sf::Drawable* drawable = *it;
         window->draw(*drawable);
     }
 
     window->display();
 
-    toDraw.clear();
+    toDrawOnMain.clear();
 }
--- src/Game/GameView.h
+++ src/Game/GameView.h
@@ -4,6 +4,11 @@
 #include <SFML/Graphics.hpp>
 
 #include <iostream>
+#include <map>
+
+enum class ViewLayer {
+    MENU, OVERWORLD, LEVEL, HUD
+};
 
 class GameView {
 public:
@@ -11,19 +16,25 @@ public:
     virtual ~GameView();
 
     void draw();
-    void addDrawable(sf::Drawable*);
+    void addDrawable(ViewLayer viewKey, sf::Drawable*);
 
     sf::RenderWindow* getWindow();
 
+    void addView(ViewLayer viewKey, sf::View* view);
+    void removeView(ViewLayer viewKey);
+
     void reset();
-    void setCameraCenter(const sf::Vector2f* pos);
-    void setCameraCenter(float x, float y);
-    void setFollowedPoint(const sf::Vector2f* pos);
-    void setFollowedPoint(float x, float y);
+    void setCameraCenter(ViewLayer viewKey, const sf::Vector2f* pos);
+    void setCameraCenter(ViewLayer viewKey, float x, float y);
+    void setFollowedPoint(ViewLayer viewKey, const sf::Vector2f* pos);
+    void setFollowedPoint(ViewLayer viewKey, float x, float y);
 protected:
 private:
+    std::map<ViewLayer, sf::View*> viewMap;
+    std::map<ViewLayer, std::vector<sf::Drawable*>> drawableMap;
+    std::vector<ViewLayer> toDraw;
+
     sf::RenderWindow* window;
-    std::vector<sf::Drawable*> toDraw;
     sf::Vector2f center;
 };
 
