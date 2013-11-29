c3697ed604a826c3a99445c1101839565d246943
Jeremy Rabasco <jeremy.rabasco@epfl.ch>
2013-11-29 03:07:21 +0100

    Removed double speed definition

--- src/Entity/Elodie.cpp
+++ src/Entity/Elodie.cpp
@@ -24,7 +24,6 @@ void Elodie::init() {
     //levelSpeed and hitboxes are set here for the moment, but it's not the right place to set them
     sf::Vector2f pnt1 = {82, 37}, pnt2 = {106, 82};
     Hitbox hitbox(pnt1, pnt2);
-    speed.x = 100;
     setCurrentHitbox(0);
     addHitbox(hitbox);
 }
