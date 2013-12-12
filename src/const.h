d6901862b559bcef1e70320abba3be7cf5626ce8
Gerardufoin <gerardufoin2@hotmail.com>
2013-12-12 02:24:05 +0100

    Corrected SOUND_TYPE_SHEEP_MAX

--- src/const.h
+++ src/const.h
@@ -99,7 +99,7 @@ const string SOUND_TYPE_SPIKES = "spikes";
 const int SOUND_TYPE_SPIKES_MAX = 2;
 
 const string SOUND_TYPE_SHEEP = "sheep";
-const int SOUND_TYPE_SHEEP_MAX = 5;
+const int SOUND_TYPE_SHEEP_MAX = 4;
 
 const string SOUND_TYPE_FOOTSTEP_GROUND = "footstep_ground";
 const int SOUND_TYPE_FOOTSTEP_GROUND_MAX = 4;
