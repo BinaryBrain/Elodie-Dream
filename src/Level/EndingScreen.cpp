98aa988a4ef7f8d85bb14c90f066a418f7ef3797
Jeremy Rabasco <jeremy.rabasco@epfl.ch>
2013-12-13 22:59:35 +0100

    Added ending screen

---
+++ src/Level/EndingScreen.cpp
@@ -0,0 +1,27 @@
+#include "EndingScreen.h"
+
+EndingScreen::EndingScreen(GameView * view, bool isMute) : Displayable(view) {
+    texture.loadFromFile("assets/img/sprites/menu/fin.png");
+    endingImage.setTexture(texture);
+    endingImage.setPosition(0,0);
+
+    view->addView(ViewLayer::ENDINGSCREEN, this);
+
+    if (!music.openFromFile(MUSIC_PATH+"/"+END_MUSIC)) {
+        // TODO Handle error
+    } else {
+        music.setLoop(false);
+
+        if(!isMute) {
+            music.play();
+        }
+    }
+
+}
+
+EndingScreen::~EndingScreen() {
+}
+
+void EndingScreen::display() {
+    gameView->addDrawable(ViewLayer::ENDINGSCREEN, &endingImage);
+}
---
+++ src/Level/EndingScreen.h
@@ -0,0 +1,25 @@
+#ifndef ENDINGSCREEN_H
+#define ENDINGSCREEN_H
+
+#include "../const.h"
+#include "../env.h"
+#include "../Displayable/Displayable.h"
+#include "../Game/GameView.h"
+
+#include <SFML/Audio.hpp>
+
+class EndingScreen : public Displayable {
+public:
+    EndingScreen(GameView* view, bool isMute);
+    virtual ~EndingScreen();
+
+    void display();
+
+private:
+    sf::Texture texture;
+    sf::Sprite endingImage;
+    sf::Music music;
+
+};
+
+#endif // DEATH_H
