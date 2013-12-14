a02e89d539120921d7c233afecaf223835a012e9
Sacha Bron <sacha.bron@copperflake.com>
2013-12-14 16:35:34 +0100

    fix bad const name

--- src/const.h
+++ src/const.h
@@ -139,7 +139,7 @@ const string SOUNDS_PATH = "assets/sounds";
 const string SOUND_TYPE_PUNCH = "punch";
 const int SOUND_TYPE_PUNCH_MAX = 2;
 
-const string SOUND_TYPE_WOOSH = "whoosh";
+const string SOUND_TYPE_WOOSH = "woosh";
 const int SOUND_TYPE_WOOSH_MAX = 1;
 
 const string SOUND_TYPE_SPIKES = "spikes";
