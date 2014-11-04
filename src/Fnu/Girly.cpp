5f8124e8c85e311fa073b43fc970ed91be8c45e8
Flagoul <basile.vu@gmail.com>
2014-11-05 00:18:57 +0100

    Optimizing Girly display

--- src/Fnu/Girly.cpp
+++ src/Fnu/Girly.cpp
@@ -1,7 +1,13 @@
 #include "Girly.h"
 
 Girly::Girly(GameView* gameView) : Displayable(gameView) {
+    float viewX = gameView->getSizeX();
+    float viewY = gameView->getSizeY();
+
     gameView->addView(ViewLayer::GIRLY, this);
+    filter.setSize(sf::Vector2f(viewX, viewY));
+    filter.setFillColor(sf::Color(0xFF, 0x14, 0x93, 0x7f));
+    filter.setPosition(0, 0);
 }
 
 Girly::~Girly() {
@@ -9,15 +15,5 @@ Girly::~Girly() {
 }
 
 void Girly::display() {
-    float viewX(gameView->getWindow()->getSize().x);
-    float viewY(gameView->getWindow()->getSize().y);
-
-    float dimX(viewX);
-    float dimY(viewY);
-
-    filter.setSize(sf::Vector2f(dimX, dimY));
-    filter.setFillColor(sf::Color(0xFF, 0x14, 0x93, 0x7f));
-    filter.setPosition(0, 0);
-
     gameView->addDrawable(ViewLayer::GIRLY, &filter);
 }
