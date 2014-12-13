0d411c75b50e6a43e1f34636cc3cdeaba21555bb
Kewin Dousse <kewin.d@websud.ch>
2014-12-13 22:03:56 +0100

    Adding Laser class

---
+++ src/Entity/Enemy/Laser.cpp
@@ -0,0 +1,67 @@
+#include "../../Sound/SoundManager.h"
+#include "Laser.h"
+
+const int Laser::DAMAGE = 50;
+const std::map< int, std::string > Laser::ANIMATIONS =
+{
+    {Laser::State::WAITING, "waiting"},
+    {Laser::State::ACTIVATED, "activating"},
+    {Laser::State::UNACTIVATED, "activated"}
+};
+
+Laser::Laser() : Laser(sf::Vector2f(0, 0))
+{
+}
+
+Laser::Laser(sf::Vector2f position) :
+    Entity(position, EntityType::ENEMY, EntityName::SPIKES,
+           ENTITYTYPE_ENEMY+"/"+ENTITYNAME_SPIKES+".png", "waiting",
+           Laser::ANIMATIONS, Laser::State::WAITING,
+{0, 0}, 1, Laser::DAMAGE)
+{
+}
+
+Laser::Laser(float x, float y) : Laser(sf::Vector2f(x, y))
+{
+}
+
+Laser::~Laser()
+{
+}
+
+void Laser::doAttack(std::map< std::string, Entity* >& entities)
+{
+    sf::FloatRect entity = getCurrentHitbox(animations.at(state), sprite->getCurrentFrame()).getArea();
+    Elodie* elodie = (Elodie*) entities["elodie"];
+    if (state == Laser::State::ACTIVATED && sprite->getCurrentFrame() == 2 &&
+            entity.intersects(elodie->returnCurrentHitbox().getArea()))
+    {
+        elodie->takeDamage(DAMAGE, true);
+    }
+    if (!activated && entity.intersects(elodie->returnCurrentHitbox().getArea()))
+    {
+        state = Laser::State::ACTIVATED;
+        changeStance(animations.at(state), sf::seconds(0.05f));
+        activated = true;
+
+        SoundManager::getInstance().play(SoundType::SPIKES);
+    }
+}
+
+void  Laser::takeDamage(int, bool)
+{
+}
+
+void Laser::doStuff(const EventHandler&, const std::vector< std::vector<TileSprite*> >&,
+                     std::map< std::string, Entity* >& entities, sf::Time animate)
+{
+    doAttack(entities);
+
+    if (state == Laser::State::ACTIVATED && sprite->getCurrentFrame() == 3)
+    {
+        state = Laser::State::UNACTIVATED;
+        changeStance(animations.at(state), sf::seconds(0.05f));
+    }
+
+    sprite->update(animate);
+}
---
+++ src/Entity/Enemy/Laser.h
@@ -0,0 +1,41 @@
+#ifndef SPIKES_H_INCLUDED
+#define SPIKES_H_INCLUDED
+
+#include "../Entity.h"
+#include "../../Sprite/EntitySprite.h"
+#include "../../Include/Collide.h"
+#include "../../Include/EntityInfo.h"
+#include "../../EventHandler/EventHandler.h"
+#include "../../EntityManager/EntityManager.h"
+#include "../Elodie.h"
+
+class Laser : public Entity
+{
+public:
+    static const int DAMAGE;
+    static const std::map< int, std::string > ANIMATIONS;
+
+    enum State
+    {
+        WAITING,
+        ACTIVATED,
+        UNACTIVATED
+    };
+
+public:
+    Laser();
+    Laser(sf::Vector2f position);
+    Laser(float x, float y);
+    virtual ~Laser();
+
+    void doAttack(std::map< std::string, Entity* >& entities);
+
+    virtual void takeDamage(int damage, bool ignore);
+    virtual void doStuff(const EventHandler& event, const std::vector< std::vector<TileSprite*> >& tiles,
+                         std::map< std::string, Entity* >& entities, sf::Time animate);
+
+private:
+    bool activated = false;
+};
+
+#endif // SPIKES_H_INCLUDED
